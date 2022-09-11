18a79e4ae00810b48f77b15aadfe451b9b5f0e92
anhpeter <webfullstack99@gmail.com>
2022-09-11 17:57:48 +0700

    add redux form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import './App.css';
+import FieldLevelValidationForm from './components/FieldLevelValidationForm';
+import SubmitValidationForm from './components/SubmitValidationForm';
+import UserForm from './components/users/UserForm';
+import UserSection from './components/users/UserSection';
 
 function App() {
-  return <div className="App">app works haha ! !</div>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+  return (
+    <div>
+      <UserSection />
+      <UserForm onSubmit={onSubmit} />
+      <SubmitValidationForm />
+      <FieldLevelValidationForm />
+    </div>
+  );
 }
 
 export default App;
