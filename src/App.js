306f74d480d775a9b7a366f679b6fe81fd72fc74
anhpeter <webfullstack99@gmail.com>
2022-09-11 14:28:03 +0700

    add !

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">app works haha</div>;
+  return <div className="App">app works haha !</div>;
 }
 
 export default App;
